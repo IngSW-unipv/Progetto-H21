b5f023e580cc695d7da992da854b25c69c427684
jessica <74250962+jessi-bit@users.noreply.github.com>
2021-07-03 20:57:17 +0200

    Update ComponentDTOtest.java

--- server/src/test/java/it/skinjobs/dto/ComponentDTOtest.java
+++ server/src/test/java/it/skinjobs/dto/ComponentDTOtest.java
@@ -15,12 +15,12 @@ public class ComponentDTOtest {
         ComponentDTO componentDTO = new ComponentDTO();
         componentDTO.setName("test_name");
         componentDTO.setFamilyId(7);
-        componentDTO.setPowerSupply(100.00f);
+        componentDTO.setPower(100.00f);
         componentDTO.setPrice(30.00f);
 
         assertEquals("test_name", componentDTO.getName());
         assertEquals(7, componentDTO.getFamilyId());
-        assertEquals(100.00f, componentDTO.getPowerSupply());
+        assertEquals(100.00f, componentDTO.getPower());
         assertEquals(30.00f, componentDTO.getPrice());
         
     
